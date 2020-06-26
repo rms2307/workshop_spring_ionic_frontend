2f269b41d5e5f0e24763a83f5b894af85c42cbc3
Robson Silva <rms2307@outlook.com>
2020-06-26 14:49:47 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -9,6 +11,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -16,8 +22,10 @@ export class CategoriasPage {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => console.log(response),
-      error => console.log(error));
+    this.categoriaService.findAll().subscribe(
+      response => { this.items = response; },
+      error => { console.log(error); }
+    );
   }
 
 }
