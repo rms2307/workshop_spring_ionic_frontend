89f207b9005f0bee2a82e226546d3c42a507f526
Robson Silva <rms2307@outlook.com>
2020-06-28 18:51:33 -0300

    Tratando erro 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -30,7 +30,13 @@ export class ProfilePage {
           this.cliente = response;
           this.getImgIfExists();
         },
-          error => { });
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
