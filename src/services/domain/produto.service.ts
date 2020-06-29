83b1d8861b34bd6ea403717106d1cc359f3af995
Robson Silva <rms2307@outlook.com>
2020-06-29 19:49:45 -0300

    Carregando as imagens nos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -12,4 +12,9 @@ export class ProdutoService {
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/search/?categorias=${categoria_id}`);
     }
+
+    getSmallImgFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, { responseType: 'blob' });
+    }
 }
