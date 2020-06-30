f951ff90b79c7f05f312d838112b32d246126597
Robson Silva <rms2307@outlook.com>
2020-06-30 16:16:39 -0300

    Tela de escolha de pagamento

--- src/pages/escolha-endereco/escolha-endereco.ts
+++ src/pages/escolha-endereco/escolha-endereco.ts
@@ -54,6 +54,7 @@ export class EscolhaEnderecoPage {
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = { id: item.id };
     console.log(this.pedido);
+    this.navCtrl.push('PagamentoPage', { pedido: this.pedido });
   }
 
 }
